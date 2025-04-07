abaa79d15de681c24792f4d7f5a11209ade6528d
vxlot <151598803+vxlot@users.noreply.github.com>
2025-04-07 20:48:34 +0800

    Add files via upload

---
+++ download_from_hf_parquet.py
@@ -0,0 +1,26 @@
+# export HF_ENDPOINT=https://hf-mirror.com
+# export HF_HOME="/ifs/root/ipa01/110/user_110003/download"
+
+from datasets import load_dataset
+from datasets import get_dataset_split_names
+
+# 这个数据集太大了 可以只下载一个parque文件测试
+dataset = load_dataset("yuvalkirstain/pickapic_v2", split="train")
+
+
+from datasets import load_dataset
+import io
+from PIL import Image
+
+
+dataset = load_dataset("parquet", data_files="/ifs/root/ipa01/110/user_110003/download/hub/datasets--yuvalkirstain--pickapic_v2/snapshots/12d45c8a6fcbc35c18a067efb24d993caaf4b8a7/data/test-00000-of-00014-387db523fa7e7121.parquet")
+print(dataset.data['train'].column_names)
+print(dataset.data['train']['caption'][0])
+
+im_scalar = dataset.data['train']['jpg_0'][0]  
+im_bytes = im_scalar.as_buffer().to_pybytes() 
+Image.open(io.BytesIO(im_bytes)).convert("RGB").save("jpg_0.png")
+
+im_scalar = dataset.data['train']['jpg_1'][0]  
+im_bytes = im_scalar.as_buffer().to_pybytes() 
+Image.open(io.BytesIO(im_bytes)).convert("RGB").save("jpg_1.png")
